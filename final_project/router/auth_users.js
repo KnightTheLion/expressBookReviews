0a58f57437a822334bb113b7bb1dc83354178c56
Joshua <josh@ktlweb.dev>
2023-12-06 21:23:58 -0500

    added all server code beofre async-await and Promises

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -2,28 +2,89 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
+const session = require('express-session')
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+const authenticatedUser = (username,password)=>{
+  let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
-
+regd_users.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+  
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60*6000 });
+  
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
+  });
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+regd_users.put('/auth/review/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.body.username; 
+    const postReview = req.body.review; 
+  
+    if (!books[isbn]) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    if (books[isbn].reviews[username]) {
+        books[isbn].reviews[username] = postReview;
+        return res.status(200).json({book: books[isbn]});
+      } else {
+        books[isbn].reviews[username] = postReview;
+        return res.status(200).json({book: books[isbn]});
+      }
+  });
+
+  regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.body.username;
+  
+    if (!books[isbn]) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+  
+    if (!books[isbn].reviews || !books[isbn].reviews[username]) {
+      return res.status(404).json({ message: 'Review not found' });
+    }
+  
+    delete books[isbn].reviews[username];
+  
+    return res.status(200).json({book: books[isbn]});
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
